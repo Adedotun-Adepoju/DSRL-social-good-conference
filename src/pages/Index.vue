8de26c266d4a2a619d0a1536b7dd2384c0d0d49c
Adedotun.Adepoju <d.e.adepoju@gmail.com>
2024-02-13 19:41:06 +0100

    add agenda section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -37,7 +37,9 @@
     </div>
     <div class="body">
       <p class="title">Data Science For Social Good Conference</p>
-      <p class="sub-title">May 20, 2024 | Morgan State University</p>
+      <p class="sub-title">Location: 212 University Student Center, Morgan State University </p>
+      <!-- <p class="sub-title">1700 East Cold Spring Lane, Baltimore, Maryland 21251</p> -->
+      <p class="date">Date: May 03, 2024</p>
     </div>
     <div class="right-divider">
     </div>
@@ -45,13 +47,14 @@
   <div class="about-section">
     <div class="topics">
       <div v-for="(item, key) in topics" :key="key" class="topic-container">
-        <p class="number">{{ key + 1 }}</p>
-        <p class="topic">{{ item.name }}</p>
+        <ul v-for="(topic,index) in item" :key="index">
+          <li class="topic">{{ topic }}</li>
+        </ul>
       </div>
     </div>
     <div class="details">
       <p class="about">About the Conference</p>  
-      <p class="description">Details to be added...</p>
+      <p class="description">Practitioner/Scholar Collaboration to Localize Addressing Social Problems, mainly with the help of Data Science, in the NJ Metro and the Baltimore/DC area.</p>
     </div>
   </div>
   <div class="speakers-section" id="speakers">
@@ -71,7 +74,20 @@
   </div>
   <div class="agenda-section" id="agenda">
     <p class="heading">Agenda</p>
-    <p class="body">To be confirmed....</p>
+    <p class="session">Morning Session</p>
+    <div v-for="(item, index) in morningAgenda" :key="index" class="schedule">
+      <p class="time">{{ item.time }}</p>
+      <hr>
+      <p class="title">{{ item.title }}</p>
+      <p class="sub">{{ item.sub }}</p>
+    </div>
+    <p class="session">Afternoon Session</p>
+    <div v-for="(item, index) in afternoonAgenda" :key="index" class="schedule">
+      <p class="time">{{ item.time }}</p>
+      <hr>
+      <p class="title">{{ item.title }}</p>
+      <p class="sub">{{ item.sub }}</p>
+    </div>
   </div>
 </div>
 </template>
@@ -107,17 +123,78 @@ export default {
         }
       ],
       topics: [
+        ["Violence Prevention","Depression and Anxiety" , "Psychological Distress", "Educational Equity"],
+        ["Criminal Recidivism", "Conflict Resolution", "Homelessness", "Domestic Abuse"],
+        ["Equity in Social Welfare Policy", "Marginalization", "Maternal Mental Health", "Opioid Use Disorders"],
+        ["Cyberbullying", "Discrimination", "Caregiving", "Reentry"]
+      ],
+      morningAgenda: [
         {
-          name: "Violence Prevention"
+          time: "8:30 AM - 9:00 AM",
+          title: "Registration and Welcome Breakfast",
+          sub: "An opportunity for attendees to mingle and enjoy light refreshments."
         },
         {
-          name: "Homelessness"
+          time: "9:00 AM - 9:15 AM",
+          title: "Opening Remarks",
+          sub: "Speaker 1"
         },
         {
-          name: "Educational Equity"
+          time: "9:15 AM - 10:00 AM",
+          title: "Keynote Address: TBD",
+          sub: "Speaker 2"
         },
         {
-          name: "Reentry"
+          time: "10:00 AM - 10:45 AM",
+          title: "Panel Discussion: ??",
+          sub: ""
+        },
+        {
+          time: "10:45 AM - 11:00 AM",
+          title: "Coffee Break",
+          sub: ""
+        },
+        {
+          time: "11:00 AM - 12:00 PM",
+          title: "Presentation 1:",
+          sub: ""
+        },
+      ],
+      afternoonAgenda: [
+        {
+          time: "12:00 PM - 1:00 PM",
+          title: " Networking Lunch",
+          sub: ""
+        },
+        {
+          time: "1:00 PM - 1:30 PM pm",
+          title: "Speaker 3",
+          sub: ""
+        },        
+        {
+          time: "2:00 PM - 2:30 PM",
+          title: "Lightning Talk Session",
+          sub: ""
+        },
+        {
+          time: "3:00-3:30pm",
+          title: "Speaker 4",
+          sub: ""
+        },
+        {
+          time: "3:30 PM - 3:45 PM",
+          title: "Coffee Break",
+          sub: ""
+        },
+        {
+          time: "3:45 PM - 4:45 PM",
+          title: "Lightning Talk Session (Continued)",
+          sub: ""
+        },
+        {
+          time: "4:45 PM - 5:00 PM",
+          title: " Closing Remarks and Acknowledgments",
+          sub: "Speaker 5"
         }
       ]
     }
